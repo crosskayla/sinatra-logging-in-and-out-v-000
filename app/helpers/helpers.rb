0ab8e46890e95f17e2e6a8c192a8c2eaa85425ab
Kayla Cross <kac288@georgetown.edu>
2018-09-05 16:56:07 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,8 @@
 class Helpers
 
   def current_user(session_hash)
-    User.find(session_hash[:user_id])
+    user = User.find(session_hash[:user_id])
+    user
   end
 
   def is_logged_in?(session_hash)
