85a95ee076e3dac5c91ff53bcb247e0fe0be16fb
Kayla Cross <kac288@georgetown.edu>
2018-09-05 17:08:48 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,9 @@ require 'sinatra/base'
 class Helpers
 
   def self.current_user(session_hash)
-    @user = User.find(session_hash[:user_id])
+    if session_hash[:user_id]
+      @user = User.find(session_hash[:user_id])
+    end
   end
 
   def self.is_logged_in?(session_hash)
